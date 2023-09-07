68a5c270c6b84990b4c6d2d4e3534ac1673617ec
Alessandro Tundo <alessandrotundo94@gmail.com>
2023-09-07 16:28:52 +0200

    Add missing parameter fro subprocess call

--- use-case-exp/iweblens/client/run_workload.py
+++ use-case-exp/iweblens/client/run_workload.py
@@ -8,7 +8,7 @@ def main():
     try:
         processes = []
         for h in hosts:
-            process = subprocess.Popen(["locust", "-f", "locust_file.py", "-h", f"{h['endpoint']}", "-u", f"{h['users']}", "-r", f"{h['rate']}", "--headless"])
+            process = subprocess.Popen(["locust", "-f", "locust_file.py", "-h", f"{h['endpoint']}", "-u", f"{h['users']}", "-r", f"{h['rate']}", "--images-folder", "inputfolder", "--headless"])
             processes.append(process)
     except KeyboardInterrupt:
         for process in processes:
